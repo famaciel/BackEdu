ffbd7e8471e267298c037dfa05ae945c159e1ad1
Fernando Andrey Maciel <31479036+famaciel@users.noreply.github.com>
2024-01-22 10:46:08 -0300

    Create EscolaSonhosAlunosMatrGetById.py

---
+++ EscolaSonhosAlunosMatrGetById.py
@@ -0,0 +1,34 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+    
+    try:
+
+        print(event)
+
+        client = boto3.resource("dynamodb")
+        table = client.Table("EscolaSonhos_AlunosMatr")
+        
+        resp = table.get_item(
+            Key={
+                'id': event['params']['path']['id']
+            }
+            )
+        
+        return resp['Item']
+    
+    except KeyError as e:
+        print(f'GOT KeyError: {e}')
+        return {
+            'statusCode': 404,
+            'headers': {},
+            'body': json.dumps({'msg': f'KeyError - {e}'})
+        }
+    except Exception as e:
+        print(f'GOT Exception: {e}')
+        return {
+            'statusCode': 500,
+            'headers': {},
+            'body': json.dumps({'msg': f'Exception - {e}'})
+        }
